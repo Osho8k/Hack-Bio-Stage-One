1088ec2a987a3852764c7bde0515cb9d9068d682
Osho8k <oshoasegh@gmail.com>
2025-11-24 10:05:34 +0100

    Hamming_Distance_Function.py

---
+++ Hamming_Distance_Function.py
@@ -0,0 +1,11 @@
+slack_username = "Aseghiemhe"
+twitter_handle = "_Oshoreame"
+
+
+def hamming_distance_calculator():
+  hamming_distance = sum(slack != twitter for slack, twitter  in zip(slack_username, twitter_handle))
+
+  #Slack and twitter are used as the variable names representing slack_username and twitter_handle to avoid repitition.
+  return(hamming_distance)
+
+print(hamming_distance_calculator())
