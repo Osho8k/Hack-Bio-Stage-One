785262b17cd966b00b4321d0351275bbef7af702
Osho8k <oshoasegh@gmail.com>
2025-11-28 09:42:14 +0100

    Simple_code.py

--- Simple_Code.py
+++ Simple_Code.py
@@ -1,3 +1,57 @@
+#DNA TO PROTEIN 
+
+dna_sequence = "ATGGCAGTC"
+
+def dna_mRNA_converter(dna_sequence):
+    # DNA to mRNA conversion
+    mRNA = ""
+    for base in dna_sequence:
+        if base == "T":
+            mRNA += "U"
+        else:
+            mRNA += base
+
+    # mRNA to codons
+    codons = [mRNA[i:i+3] for i in range(0, len(mRNA), 3)]
+
+    # Translation table
+    codon_table = {
+        "GCU": "A", "GCC": "A", "GCA": "A", "GCG": "A",
+        "UGU": "C", "UGC": "C",
+        "GAU": "D", "GAC": "D",
+        "GAA": "E", "GAG": "E",
+        "UUU": "F", "UUC": "F",
+        "GGU": "G", "GGC": "G", "GGA": "G", "GGG": "G",
+        "CAU": "H", "CAC": "H",
+        "AUA": "I", "AUU": "I", "AUC": "I",
+        "AAA": "K", "AAG": "K",
+        "UUA": "L", "UUG": "L", "CUU": "L", "CUC": "L", "CUA": "L", "CUG": "L",
+        "AUG": "M",
+        "AAU": "N", "AAC": "N",
+        "CCU": "P", "CCC": "P", "CCA": "P", "CCG": "P",
+        "CAA": "Q", "CAG": "Q",
+        "CGU": "R", "CGC": "R", "CGA": "R", "CGG": "R", "AGA": "R", "AGG": "R",
+        "UCU": "S", "UCC": "S", "UCA": "S", "UCG": "S", "AGU": "S", "AGC": "S",
+        "ACU": "T", "ACC": "T", "ACA": "T", "ACG": "T",
+        "GUU": "V", "GUC": "V", "GUA": "V", "GUG": "V",
+        "UGG": "W",
+        "UAU": "Y", "UAC": "Y",
+        "UAA": "_", "UAG": "_", "UGA": "_"
+    }
+
+    # mRNA to protein
+    protein = ""
+    for codon in codons:
+        protein += codon_table.get(codon, "?") + " "
+
+    return protein   
+
+
+print(dna_mRNA_converter(dna_sequence))
+
+
+
+
 #HAMMING DISTANCE 
 
 slack_username = "Aseghiemhe"
